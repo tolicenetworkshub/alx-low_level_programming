defb52299860f683568b5a41193c770c3a8b9668
Tolice Networks Hub <tolugbenga72@gmail.com>
2023-02-16 19:09:22 -0800

    great language

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a program that can print a line using the print f function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
