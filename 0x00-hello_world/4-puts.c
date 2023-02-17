c01cc5a017529bd88a72f6302f935599f67ed33d
Tolice Networks Hub <tolugbenga72@gmail.com>
2023-02-16 17:05:04 -0800

    my betty friend

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ *  main - A very special program thta depicts the puts function in a very special as shownin 
+ *  Return: 0 (Excellent)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
