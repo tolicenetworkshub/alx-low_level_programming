533312e17ba66632587a2342ee95ebda72cf024e
Tolice Networks Hub <tolugbenga72@gmail.com>
2023-02-20 07:56:45 -0800

    comber

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,29 +11,29 @@ int main(void)
 	int c = 0;
 	int f_d;
 
-	int 1_d;
+	int l_d;
 
 	int c2;
 	int f_d2;
 
-	int 1_d2;
+	int l_d2;
 
 	while (c <= 98)
 	{
 		f_d = (c / 10 + '0');
-		1_d = (c % 10 + '0');
+		l_d = (c % 10 + '0');
 		c2 = 0;
 		while (c2 <= 98)
 		{
 			f_d2 = (c2 / 10 + '0');
-			1_d2 = (c2 % 10 + '0');
+			l_d2 = (c2 % 10 + '0');
 			if (c < c2)
 			{
 				putchar(f_d);
-				putchar(1_d);
+				putchar(l_d);
 				putchar(' ');
 				putchar(f_d2);
-				putchar(1_d2);
+				putchar(l_d2);
 
 				if (c != 98)
 				{
