e752f69d16f6fdc39e564b1a6b60863155c6faf2
Tolice Networks Hub <tolugbenga72@gmail.com>
2023-02-22 20:52:21 -0800

    updated

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_to_90(0);
+	print_to_90(98);
+	print_to_90(111);
+	print_to_90(81);
+	print_to_90(-10);
+	return (0);
+}
