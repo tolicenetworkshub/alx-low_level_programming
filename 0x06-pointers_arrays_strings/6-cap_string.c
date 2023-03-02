7e5f93781d8e42ac1b88451b3ab70066a7c0991e
Tolice Networks Hub <tolugbenga72@gmail.com>
2023-03-02 04:36:47 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: The string to be captalized.
+ * Return: a pointer to the changes string
+ */
+char *cap_string(chair *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
+			str[index] == 32;
+		index++;
+	}
+	return (str);
+}
+
+
