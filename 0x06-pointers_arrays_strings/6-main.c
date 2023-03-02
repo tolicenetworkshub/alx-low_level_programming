d36d374bd258347fb7b00ea3ade71a36ff8d2047
Tolice Networks Hub <tolugbenga72@gmail.com>
2023-03-02 11:08:53 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,11 +8,11 @@
  */
  int main(void)
 {
-	char str[] = "expected the best. prepare for the worst. capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n;
+	char str[] = "Expected the best. prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n;
 	char *ptr;
 
 	ptr = cap_string(str);
-	printf("%s"; ptr);
+	printf("%s"; str);
 	printf("%s", str);
 	return (0);
 }
