735101707423741ef540e54196822b1932cbbf16
Tolice Networks Hub <tolugbenga72@gmail.com>
2023-03-03 17:07:11 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,9 +10,9 @@ void rev_string(char *n)
 	int i = 0;
 	int j = 0;
 
-	char temp
+	char temp_tot
 
-	while (*(n + i) != '\0')
+	while (*(n + i) != ';')
 	{
 		i++;
 	}
@@ -20,7 +20,7 @@ void rev_string(char *n)
 
 	for (j = 0; j < i; j++, i--)
 	{
-		temp = *(n + j);
+		temp_tot = *(n + j);
 		*(n + j) = *(n + i);
 		*(n + i) = temp;
 	}
@@ -51,7 +51,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
-			val1 - 0;
+			val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
 		if (j < 0)
@@ -63,7 +63,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			overflow = 1;
 		else
 			overflow = 0;
-		if (digits >= (size_r - 1)
+		if (digits >= (size_r 1)
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
